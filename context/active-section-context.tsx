474c5573d10c957caa1e0b070961100ff52e1e8a
DSDeekshith <dasarideekshith321@gmail.com>
2024-03-06 00:14:57 +0530

    Changes related Contextual components

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,11 +1,34 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import { links } from "@/lib/data";
 
 type SectionName = (typeof links)[number]["name"];
+type ActiveSectionContextProviderProps = {
+  children: React.ReactNode;
+};
 
-export default function ActiveSectionContextProvider() {
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+};
+
+const ActiveSectionContext = createContext<ActiveSectionContextType | null>(
+  null
+);
+
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
 
-  return <div>ActiveSectionContextProvider</div>;
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
 }
